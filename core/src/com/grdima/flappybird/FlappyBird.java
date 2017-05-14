1bad417569ab8513906e06766d95fcde0306b48c
Dmitry Herutskyi <gerutskyi@gmail.com>
2017-05-14 21:50:45 +0300

    Menu texture render implementation

--- core/src/com/grdima/flappybird/FlappyBird.java
+++ core/src/com/grdima/flappybird/FlappyBird.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.grdima.flappybird.states.GameStateManager;
+import com.grdima.flappybird.states.MenuState;
 
 public class FlappyBird extends ApplicationAdapter {
 	public static final int WIDTH = 480;
@@ -24,6 +25,7 @@ public class FlappyBird extends ApplicationAdapter {
 		batch = new SpriteBatch();
         gsm = new GameStateManager();
         Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.push(new MenuState(gsm));
 	}
 
 	@Override
--- core/src/com/grdima/flappybird/states/MenuState.java
+++ core/src/com/grdima/flappybird/states/MenuState.java
@@ -1,14 +1,22 @@
 package com.grdima.flappybird.states;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.grdima.flappybird.FlappyBird;
 
 /**
  * Created by Dmitry Herutskyi on 14.05.17.
  */
 
 public class MenuState extends State {
+
+    private Texture background;
+    private Texture playButton;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playButton = new Texture("playbtn.png");
     }
 
     @Override
@@ -23,11 +31,15 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGTH);
+        sb.draw(playButton, (FlappyBird.WIDTH / 2 - playButton.getWidth() / 2), FlappyBird.HEIGTH / 2);
+        sb.end();
     }
 
     @Override
     public void dispouse() {
-
+        background.dispose();
+        playButton.dispose();
     }
 }
