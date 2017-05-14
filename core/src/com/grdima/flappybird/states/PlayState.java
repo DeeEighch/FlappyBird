8669a7606c00da5e216e89084f3c1085c4a74514
Dmitry Herutskyi <gerutskyi@gmail.com>
2017-05-14 21:58:48 +0300

    Added empty PlayState class

---
+++ core/src/com/grdima/flappybird/states/PlayState.java
@@ -0,0 +1,34 @@
+package com.grdima.flappybird.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Dmitry Herutskyi on 14.05.17.
+ */
+
+public class PlayState extends State {
+
+    public PlayState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispouse() {
+
+    }
+}
